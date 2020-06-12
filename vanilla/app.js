1cb43bd4af23057f9541c9000959f7f5789c37cb
Daniel Stocks <daniel@webcloud.se>
2020-06-12 15:55:51 +0200

    prettier

--- vanilla/app.js
+++ vanilla/app.js
@@ -3,9 +3,9 @@ import { TodoListContainer } from "shared";
 import { TodoList } from "./todo-list";
 
 export default function App() {
-	return (
-		<TodoListContainer>
-			<TodoList />
-		</TodoListContainer>
-	);
+  return (
+    <TodoListContainer>
+      <TodoList />
+    </TodoListContainer>
+  );
 }
